bdfcef4f42e25b847adbdd468bfef661ea105341
Jerry Jacob <jerry679@revature.net>
2022-06-03 12:48:02 -0400

    added admin component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Home from "./components/home";
 import Menu from "./components/menu";
+import Admin from "./components/admin";
+
+export const userContext = createContext();
 
 function App() {
+  const [user, setUser] = useState({ uname: "Guest" });
   return (
     <> 
       <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route exact path = "" element = {<Home />} />
-          <Route path = "menu" element = {<Menu />} />
-        </Routes>
+        <userContext.Provider value={[user, setUser]}>
+            <NavBar />
+            <Routes>
+              <Route exact path = "" element = {<Home />} />
+              <Route path = "menu" element = {<Menu />} />
+              <Route path = "admin" element = {<Admin />} />
+            </Routes>
+          </userContext.Provider>
       </BrowserRouter>
     </>
   );
---
+++ src/components/admin.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useContext, useRef } from "react";
+import { userContext } from "../App";
+
+export default function Admin() {
+    const unameInput = useRef();
+    const pwInput = useRef();
+    const [user, setUser] = useContext(userContext);
+    
+    async function login() {
+        // Whenever you are getting a useRefs value, make sure it's inside some function call. Otherwise it will
+        // error due to the refInput.current = undefined, meaning there is no .value available
+        const admin = {
+            username: unameInput.current.value,
+            password: pwInput.current.value,
+        };
+
+        try {
+            const response = await axios.post(`http://localhost:8080/mulberry/admin`, admin);
+            console.log(response.data);
+            setUser({ ...user, uname: admin.uname });
+            console.log("This is after we set the user ", user);
+            } catch (error) {
+                console.error(error.response.data);
+                alert(error.response.data);
+            }
+        
+    }
+
+    return (
+        <>
+            <h4>Welcome back, please log in below.</h4>
+            <input placeholder="Enter username" ref={unameInput}></input>
+            <input type="password" placeholder="Enter password" ref={pwInput}></input>
+            <button onClick={login}>Login</button>
+        </>
+    )
+}
